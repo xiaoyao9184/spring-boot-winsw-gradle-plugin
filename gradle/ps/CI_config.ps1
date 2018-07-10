252a9a2e24a92cd9c7250f72455b276250b55082
xiaoyao9184 <xiaoyao9184@gmail.com>
2018-07-10 16:12:25 +0800

    Add config for CI and Deploy Server

---
+++ gradle/ps/CI_config.ps1
@@ -0,0 +1,63 @@
+﻿#ServerName
+
+param($ComputerName)
+#$ComputerName="*"
+
+if(-Not $ComputerName){
+    if($env:Computer){
+        $ComputerName = $env:Computer
+    }else{
+        Write-Host "Missing argument 'ComputerName', cant run this script!"
+        return
+    }
+}
+
+
+#Starting WinRM
+
+$service = Get-Service "WinRM"
+if (!$service)
+{
+    Write-Host -ForegroundColor Red "WinRM not exist!" 
+    return
+}
+if($service.Status -eq "Running")
+{
+    Write-Host "WinRM already start" 
+}
+else
+{
+    Write-Host "Starting WinRM..."
+    Start-Service "WinRM"
+}
+
+
+#Get TrustedHosts
+
+$TrustedHosts = Get-Item wsman:\localhost\Client\TrustedHosts
+
+if($TrustedHosts.Value)
+{
+    $TrustedHostList = $TrustedHosts.Value.Split(",")
+}
+else
+{
+    $TrustedHostList = @()
+}
+
+
+if($TrustedHostList -contains $ComputerName)
+{
+    Write-Host "TrustedHosts already has this remote host:" $ComputerName
+    return
+}
+
+
+#Add TrustedHosts
+
+$TrustedHostList += $ComputerName
+$TrustedHosts = $TrustedHostList -join ','
+
+Write-Host "TrustedHosts will be set to:" $TrustedHosts
+
+Set-Item wsman:\localhost\Client\TrustedHosts -value $TrustedHosts
---
+++ gradle/ps/Deploy_config.ps1
@@ -0,0 +1,23 @@
+﻿#Starting WinRM
+
+$service = Get-Service "WinRM"
+if (!$service)
+{
+    Write-Host -ForegroundColor Red "WinRM not exist!" 
+    return
+}
+if($service.Status -eq "Running")
+{
+    Write-Host "WinRM already start" 
+}
+else
+{
+    Write-Host "Starting WinRM..."
+    Start-Service "WinRM"
+}
+
+
+#Enable PSRemoting
+Write-Host "Enable PSRemoting...".
+
+Enable-PSRemoting -Force
